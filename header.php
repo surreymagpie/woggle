0c1852205e73313e22bf42369c6dc5c27a13f1f6
Rob Curtis <surreymagpie@yahoo.co.uk>
2019-05-06 20:45:10 +0100

    Style header area

--- header.php
+++ header.php
@@ -27,7 +27,9 @@
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
-			the_custom_logo();
+			the_custom_logo(); ?>
+			<div class="site-branding__text">
+			<?php
 			if ( is_front_page() && is_home() ) :
 				?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
@@ -42,6 +44,7 @@
 				?>
 				<p class="site-description"><?php echo $woggle_description; /* WPCS: xss ok. */ ?></p>
 			<?php endif; ?>
+			</div><!-- .site-branding__text -->
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
