03389348aa40c27a51111841cca9e8a5d90a12f7
Rob Curtis <surreymagpie@yahoo.co.uk>
2019-05-17 23:02:29 +0100

    Set up CSS compilation task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+const { dest, series, src, task } = require('gulp');
+const sass = require ('gulp-sass');
+
+function css() {
+	return src('./sass/woggle.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(dest('./css/'))
+};
+
+exports.css = css;
+exports.default = css;
