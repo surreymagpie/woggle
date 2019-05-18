79f465046f34d321eedb44fd19a714a95d994f73
Rob Curtis <surreymagpie@yahoo.co.uk>
2019-05-18 14:28:36 +0100

    Use BrowserSync to reload and stream changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,24 @@
-const { dest, series, src, task, watch } = require('gulp');
+const { dest, parallel, series, src, task, watch } = require('gulp');
 const sass = require ('gulp-sass');
+const browserSync = require('browser-sync').create();
 
 function css() {
 	return src('./sass/woggle.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(dest('./css/'))
+		.pipe(browserSync.stream());
 };
 
 function watch_files() {
 	watch('sass/**/*.s(a|c)ss', css);
 }
 
+function browsersync(done) {
+    browserSync.init({
+        proxy: "http://localhost:8080"
+	});
+	done();
+};
+
 exports.css = css;
-exports.default = series(css, watch_files);
+exports.default = series(css, browsersync, watch_files);
