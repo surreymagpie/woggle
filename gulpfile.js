47ed2fdf6c48f2614a57ac091ad1f9a79d9b3231
Rob Curtis <surreymagpie@yahoo.co.uk>
2019-05-18 12:47:09 +0100

    Create watch task for scss and sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { dest, series, src, task } = require('gulp');
+const { dest, series, src, task, watch } = require('gulp');
 const sass = require ('gulp-sass');
 
 function css() {
@@ -7,5 +7,9 @@ function css() {
 		.pipe(dest('./css/'))
 };
 
+function watch_files() {
+	watch('sass/**/*.s(a|c)ss', css);
+}
+
 exports.css = css;
-exports.default = css;
+exports.default = series(css, watch_files);
